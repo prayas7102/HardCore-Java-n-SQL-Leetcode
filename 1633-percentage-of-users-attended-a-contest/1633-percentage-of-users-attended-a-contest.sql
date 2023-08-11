2bda8f78875389e0295f2c0ba96595c8600f9b10
Prayas Kumar <71717433+prayas7102@users.noreply.github.com>
2023-08-12 00:25:35 +0530

    Time: 1801 ms (75.05%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -3,4 +3,4 @@ SELECT
     ROUND((COUNT(user_id) * 100 / (SELECT COUNT(user_id) FROM Users)), 2) AS percentage
 FROM Register
 GROUP BY contest_id
-ORDER BY percentage DESC,contest_id;
+ORDER BY percentage DESC, contest_id ASC;
