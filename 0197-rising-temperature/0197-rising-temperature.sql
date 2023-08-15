b33057085dd8e5f0e4e2bceb4f5c3ca8bb0ac9a3
Prayas Kumar <71717433+prayas7102@users.noreply.github.com>
2023-08-15 12:50:57 +0530

    Update 0197-rising-temperature.sql

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
+# comparing row of same table
 select w1.id from Weather w1, Weather w2 where datediff(w1.recordDate,w2.recordDate)=1 and
 w1.temperature>w2.temperature;
